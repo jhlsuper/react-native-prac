7d413efade3ff5cee474544b38daffd1ea0f38b3
파인트리 <pinetree@painteuliui-MacBookPro.local>
2021-11-19 13:36:45 +0900

    문자 인증 시스템

--- smsAuth/App.js
+++ smsAuth/App.js
@@ -1,19 +1,9 @@
-import React, {useEffect, useState} from 'react';
-import {
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  Button,
-  TextInput,
-} from 'react-native';
+import React from 'react';
+
 import {NavigationContainer} from '@react-navigation/native';
-import auth from '@react-native-firebase/auth';
-import SmsListener from 'react-native-android-sms-listener';
+
 import {createStackNavigator} from '@react-navigation/stack';
 import phoneNumber from './components/phoneNumber';
-import fakeConfirm from './components/fakeConfirm';
-import realConfirm from './components/realConfirm';
 
 const App = () => {
   const Stack = createStackNavigator();
@@ -22,8 +12,6 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Phone" component={phoneNumber} />
-        <Stack.Screen name="Fake" component={fakeConfirm} />
-        <Stack.Screen name="Real" component={realConfirm} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- smsAuth/components/phoneNumber.js
+++ smsAuth/components/phoneNumber.js
@@ -1,47 +1,44 @@
 import React, {useEffect, useState} from 'react';
-import {
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  Button,
-  TextInput,
-} from 'react-native';
+import {StyleSheet, Text, View, Button, TextInput} from 'react-native';
 import auth from '@react-native-firebase/auth';
-import SmsListener from 'react-native-android-sms-listener';
+
 const phoneNumber = ({navigation}) => {
   const [confirm, setConfirm] = useState(null);
   const [phoneNumber, addPhoneNumber] = useState('+82');
   const [code, setCode] = useState('');
   const [user, setUser] = useState();
   const [logged, setLogged] = useState(false);
+  const [get, setGet] = useState('인증번호 받기');
+  const [status, setStatus] = useState('인증 미완료');
+
   useEffect(() => {
-    console.log('receiving');
-    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-    SmsListener.addListener(message => {
-      console.log(message);
-    }, []);
-    return subscriber;
+    auth().onAuthStateChanged(onAuthStateChanged);
+
+    if (user == null) {
+      setStatus('인증 미완료.');
+    } else {
+      setStatus('인증 완료!!!');
+      setGet('인증번호 받기');
+    }
   });
   async function signInWithPhoneNumber(phoneNumber) {
+    setGet('재전송');
     const confirmation = await auth().signInWithPhoneNumber(phoneNumber);
     setConfirm(confirmation);
   }
 
   async function confirmCode() {
-    console.log('User', user);
     if (user) {
       console.log('====================================');
-      console.log('가짜가짜!!!');
+      console.log('오토 인증 완료');
       console.log('====================================');
     } else {
       try {
         await confirm.confirm(code);
         setLogged(true);
-        console.log('confirmed');
+        console.log('인증완료 되었습니다.');
       } catch (error) {
         console.log(error);
-        console.log('Invalid code.');
       }
     }
   }
@@ -49,51 +46,68 @@ const phoneNumber = ({navigation}) => {
     setUser(user);
   }
 
-  async function requestReadSmsPermission() {
-    try {
-      await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.READ_SMS,
-        {
-          title: '(...)',
-          message: "Why you're asking for...",
-        },
-      );
-    } catch (err) {}
-  }
-
-  console.log('User', user);
-  console.log('Logged', logged);
-  requestReadSmsPermission();
   const SignOut = () => {
     auth().signOut();
+    setGet('인증번호 받기');
   };
+  console.log('User', user);
 
   return (
     <View style={styles.container}>
-      <TextInput
-        placeholder="전화번호 입력"
-        onChangeText={phoneNumber => addPhoneNumber(phoneNumber)}
-        defaultValue={phoneNumber}
-      />
-      <Button
-        title="Phone Number Sign In"
-        onPress={() => signInWithPhoneNumber(phoneNumber)}
-      />
-      <TextInput
-        style={styles.color}
-        value={code}
-        onChangeText={text => setCode(text)}
-      />
-      <Button title="Confirm Code" onPress={() => confirmCode()} />
-      <Button style={styles.color} title="로그아웃" onPress={() => SignOut()} />
+      <Text style={styles.highlight}>{status}</Text>
+      <View style={styles.numberContainer}>
+        <TextInput
+          placeholder="전화번호 입력"
+          onChangeText={phoneNumber => addPhoneNumber(phoneNumber)}
+          defaultValue={phoneNumber}
+        />
+
+        <Button
+          style={styles.button}
+          title={get}
+          onPress={() => signInWithPhoneNumber(phoneNumber)}
+        />
+      </View>
+      <View style={styles.authContainer}>
+        <TextInput
+          placeholder="인증번호 입력"
+          value={code}
+          keyboardType={'numeric'}
+          onChangeText={text => setCode(text)}
+        />
+        <Button
+          style={styles.button}
+          title="인증하기"
+          onPress={() => confirmCode()}
+        />
+      </View>
+      <View>
+        <Button
+          style={styles.logout}
+          title="로그아웃"
+          onPress={() => SignOut()}
+        />
+      </View>
     </View>
   );
 };
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 5,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  numberContainer: {
+    marginTop: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  authContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 30,
   },
   color: {
     marginTop: 20,
@@ -101,17 +115,16 @@ const styles = StyleSheet.create({
     borderBottomColor: 'gray',
     borderBottomWidth: 5,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  button: {
+    paddingLeft: 20,
+    marginLeft: 40,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  logout: {
+    marginTop: 50,
   },
+
   highlight: {
-    fontWeight: '700',
+    fontSize: 50,
   },
 });
 export default phoneNumber;
